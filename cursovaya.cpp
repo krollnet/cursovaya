c68031bd07450108f1e8d98e552a4fe627cbeeb2
fpv-v <fpv-v@LAPTOP-ANEE5IE7>
2022-12-07 20:06:48 +0300

    Добавьте файлы проекта.

---
+++ cursovaya.cpp
@@ -0,0 +1,32 @@
+﻿// cursovaya.cpp: определяет точку входа для приложения.
+//
+
+#include "cursovaya.h"
+
+using namespace std;
+
+int main() {
+    TgBot::Bot bot("5900512590:AAG2B26Vyf-sUu2cYN_ekUpNx7m5JulVrcY");
+    bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
+        bot.getApi().sendMessage(message->chat->id, "Hi!");
+        });
+    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
+        printf("User wrote %s\n", message->text.c_str());
+        if (StringTools::startsWith(message->text, "/start")) {
+            return;
+        }
+        bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text);
+        });
+    try {
+        printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
+        TgBot::TgLongPoll longPoll(bot);
+        while (true) {
+            printf("Long poll started\n");
+            longPoll.start();
+        }
+    }
+    catch (TgBot::TgException& e) {
+        printf("error: %s\n", e.what());
+    }
+    return 0;
+}
